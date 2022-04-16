c1306bd91a4ac63121a040f19f93a20c0f952e7d
Daniel Jones <88003602+Soulforged187@users.noreply.github.com>
2022-04-16 14:22:43 -0600

    add ability to change views

--- src/controller/Reports.java
+++ src/controller/Reports.java
@@ -1,11 +1,29 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Reports implements Initializable {
+    Parent scene;
+    Stage stage;
+
+    public void backButtonHandler(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Schedule.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
--- src/controller/Schedule.java
+++ src/controller/Schedule.java
@@ -1,11 +1,53 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Schedule implements Initializable {
+    Parent scene;
+    Stage stage;
+
+    public void addButtonHandler(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/AddAppointment.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    public void updateButtonHandler(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/UpdateAppointment.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    public void customerButtonHandler(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/CustomerData.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    public void reportsButtonHandler(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Reports.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    public void deleteButtonHandler(ActionEvent actionEvent) throws IOException {
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
