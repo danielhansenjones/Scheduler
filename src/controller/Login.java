21cc4ed4eb3292be57d763df2a42a98c2a7da7dc
Daniel Jones <88003602+Soulforged187@users.noreply.github.com>
2022-04-24 17:29:07 -0600

    stitched screens together.

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,14 +1,17 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,6 +34,10 @@ public class Login implements Initializable {
 
     }
 
-    public void loginButtonHandler(ActionEvent actionEvent) {
+    public void loginButtonHandler(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Schedule.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,15 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-// FUTURE ENHANCEMENT
-// The largest thing that I would like to see for the future of this project is more styling added to the FXML files,
-// currently the screens are very functional but could be more aesthetically pleasing, the ability to resize windows and have the program resize
-// automatically with the user would be a great add.
 
 public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Schedule.fxml"));
-        primaryStage.setScene(new Scene(root, 900, 400));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
